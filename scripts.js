7c8d1b0736d11abec9157a91e38e82f5dc7df292
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-04-30 00:27:18 -0400

    trying to fix css, added button that prompts new grid size and clears last grids

--- scripts.js
+++ scripts.js
@@ -2,22 +2,45 @@ const container = document.querySelector("div");
 
 
 //create a loop maybe for creating 16x16 divs, or 256 total
-for(i = 0; i < 256; i++){
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "testDivs");
+let currentDimension = 256
 
-    newDiv.addEventListener("mouseenter", ()=>{
-        newDiv.style.backgroundColor = "lightgray";
-    });
-    newDiv.addEventListener("mouseleave", ()=>{
-        newDiv.style.backgroundColor = "white";
-    })
-
-    container.appendChild(newDiv);
+function drawGrid(){
+    for(i = 0; i < currentDimension; i++){
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "testDivs");
+    
+        newDiv.addEventListener("mouseenter", ()=>{
+            newDiv.style.backgroundColor = "lightgray";
+        });
+        newDiv.addEventListener("mouseleave", ()=>{
+            newDiv.style.backgroundColor = "white";
+        })
+    
+        container.appendChild(newDiv);
+    }
 }
 
 
-    
+//used to selecting new grid size
+const popUpButton = document.createElement("button");
+popUpButton.textContent = "New Grid Size";
+popUpButton.addEventListener("click", ()=>{
+    currentDimension = prompt("Enter new Grid dimension")
+    currentDimension = currentDimension * currentDimension
+    changeGrid()
+})
+document.body.appendChild(popUpButton)
    
 
+function changeGrid(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    drawGrid()
+
+
+    alert("dun")
+}
+
+drawGrid()
 console.log(container.children.length)
